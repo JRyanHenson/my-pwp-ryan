f264404f30e646077025907c78916ae2ce161bb5
JRyanHenson <hensojr@gmail.com>
2017-07-14 12:29:32 -0600

    Initial commit and push for PWP Milestone 1 documentation page.

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Milestone-1</title>
+	</head>
+	<body>
+		<h1>My Personal Website (PWP) Milestone 1</h1>
+		<h2>Purpose</h2>
+			<p>...</p>
+		<h2>Audience</h2>
+			<p>...</p>
+		<h2>Goal</h2>
+			<p>...</p>
+	</body>
+</html>
