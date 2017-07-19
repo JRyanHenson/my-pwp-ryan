f9bca7d39a1950447445390be89014f9ee55dc23
JRyanHenson <hensojr@gmail.com>
2017-07-19 16:45:16 -0600

    Began to fill in applicable data into previously created shell.

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -2,15 +2,25 @@
 <html lang="en">
 	<head>
 		<meta charset="UTF-8">
+		<meta name="My personal Website (PWP) Milestone-1" content="This is a documentation page for my PWP explaining the basic design choices for this website">
 		<title>My Personal Webstie (PWP) Milestone-1</title>
 	</head>
 	<body>
-		<h1>My Personal Website (PWP) Milestone 1</h1>
+		<h1>Ryan Henson's Personal Website (PWP) Milestone 1</h1>
 		<h2>Purpose</h2>
-			<p>...</p>
+			<p>The purpose of this site is to show off my web development skills and highlight some on my experience and career successes.</p>
 		<h2>Audience</h2>
-			<p>...</p>
+			<p>The audience for this site is potential consulting or freelance clients. Additionally, if I was looking at new opportunities for full time work this site will be good landing page for potential employers.</p>
 		<h2>Goal</h2>
-			<p>...</p>
+			<p>To serve as a gallery to showcase and promote my development skills and promote my experience in the hope of getting consulting and freelance work. </p>
+		<h2>Persona</h2>
+			<p>Name: CEO of Vulnerable Corp.  <p>
+			<p>Age: 62</p>
+			<p>Technology: </p>
+			<p>Attitudes and Behaviors: ...</p>
+			<p>Frustrations and Needs: ...</p>
+			<p>Goals: ...</p>
+			<p>User Story: ...</p>
+		<h2>Use Case</h2>
 	</body>
 </html>
