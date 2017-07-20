76090e8b814ba808e1085f32e816ef17adb108e3
JRyanHenson <hensojr@gmail.com>
2017-07-19 21:18:37 -0600

    Continued to fill in applicable data into previously created shell.

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -6,21 +6,22 @@
 		<title>My Personal Webstie (PWP) Milestone-1</title>
 	</head>
 	<body>
-		<h1>Ryan Henson's Personal Website (PWP) Milestone 1</h1>
+		<h1><center>Ryan Henson's Personal Website (PWP) Milestone-1</center></h1>
 		<h2>Purpose</h2>
-			<p>The purpose of this site is to show off my web development skills and highlight some on my experience and career successes.</p>
+			<p>The purpose of this site is to exhibit my information security experience, development skills, and highlight some of my career successes.</p>
 		<h2>Audience</h2>
-			<p>The audience for this site is potential consulting or freelance clients. Additionally, if I was looking at new opportunities for full time work this site will be good landing page for potential employers.</p>
+			<p>The audience for this site are potential consulting or freelance clients. Additionally, if I was looking at new opportunities for full time work this site will be good landing page for potential employers.</p>
 		<h2>Goal</h2>
-			<p>To serve as a gallery to showcase and promote my development skills and promote my experience in the hope of getting consulting and freelance work. </p>
+			<p>To serve as a gallery to showcase and promote my development skills and promote my experience in the information security field with the goal of obtaining consulting, freelance work, or a new full time job. </p>
 		<h2>Persona</h2>
-			<p>Name: CEO of Vulnerable Corp.  <p>
-			<p>Age: 62</p>
-			<p>Technology: </p>
-			<p>Attitudes and Behaviors: ...</p>
-			<p>Frustrations and Needs: ...</p>
-			<p>Goals: ...</p>
-			<p>User Story: ...</p>
+			<p><strong>Name:</strong> Bob B. McCobb, CEO of Vulnerable Corp.<p>
+			<p><strong>Age:</strong> 62</p>
+			<p><strong>Technology:</strong> Windows novice user. Bob uses a Dell Optiplex 620 running Windows XP, and an flip phone with a limited data plan and a limited text plan.</p>
+			<p><strong>Attitudes and Behaviors:</strong> Bob is an intelligent, business minded baby boomer. He owns a small wealth management firm with 10 employees. He has had a hard time adjusting to the modern business technology. However, he is an avid reader and understands that his business is vulnerable to malicious attacks and is seeking the services of an information security consultant in order to complete a risk based security analysis of his business.</p>
+			<p><strong>Frustrations and Needs:</strong> Bob is frustrated by the lack of local consultants willing to take on a small business contract. He needs an experienced consultant who has both a technical background and understands risk analysis and security control frameworks.</p>
+			<p><strong>Goals:</strong> Bob wants to finds a capable consultant with proven experience. He is comfortable with simple easy to use websites that clearly state the relevant information about the consultant.</p>
+			<p><strong>Story:</strong> Bob has become increasingly concerned about the possibility of ransomewear or website defacement. He feels an attack could have negative effect on this firm's reputation. He has begun to surf the web is search of the right consultant assess risk on his network and provide recommendations.</p>
 		<h2>Use Case</h2>
+			<p>Bob has researched a number of information security consultant's websites. Ryan's personal site seems to have the right feel. After deciding to explore Ryan's site further, he begins the process of reviewing the quality of the site and Ryan's highlighted credentials and experience.</p>
 	</body>
 </html>
