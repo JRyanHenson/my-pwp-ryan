fad9e0b5287a3016edf86fb123faa307d35e53b7
JRyanHenson <hensojr@gmail.com>
2017-08-02 23:54:32 -0600

    Added content to milestone-2.

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -8,10 +8,14 @@
 	<body>
 		<h1>Ryan Henson's Personal Website (PWP) Milestone-2</h1>
 		<h2>Planned Featured Content</h2>
-		<p>...</p>
+		<p>My site will be one page, and will consist of the following sections:</p>
+		<p><strong>Name and Professional Picture: </strong>This section will have my name professional title and a professional photo.</p>
+		<p><strong>About Me: </strong>This section will have a brief description of me including my aspirations and professional interests.</p>
+		<p><strong>Professional Experience: </strong>This section will have my a brief summary of my professional experience and technical skill set. It will also have button links to me LinkedIn and GitHub accounts.</p>
+		<p><strong>Contact: </strong>This section will contain a simple contact form linking to my professional email address.</p>
 		<h2>Purpose and Strategy of Featured Content</h2>
-		<p>...</p>
+		<p>To serve as a gallery to showcase and promote my development skills and promote my experience in the information security field with the goal of obtaining consulting, freelance work, or a new full time job.</p>
 		<h2>Wireframes</h2>
-		<p>...</p>
+		<img src="/~jhenson9/my-pwp-ryan/public_html/images/wireframes.png" width="800" height="500" alt="pwp2_wirframe">
 	</body>
 </html>
