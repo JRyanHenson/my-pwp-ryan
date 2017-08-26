9181d8e17343f6a70477d0d7f07e7a09281420f0
JRyanHenson <hensojr@gmail.com>
2017-08-26 11:49:53 -0600

    Added custome js files and worked on formatting.

---
+++ index.php
@@ -0,0 +1,175 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/html" xmlns="http://www.w3.org/1999/html">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+		<!-- Font Awesome -->
+		<link type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" />
+
+		<!-- Custom CSS Goes HERE -->
+		<link rel="stylesheet" href="css/style.css" type="text/css">
+
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+		<!-- jQuery - required for Bootstrap Components -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
+
+		<!-- particleground -->
+		<script src="js/jquery.particleground.min.js" type="text/javascript"></script>
+
+		<!-- not an easter egg -->
+		<script src="https://cdn.rawgit.com/mikeflynn/egg.js/master/egg.min.js"> type="text/javascript" </script>
+		<script src="js/egg.js" type="text/javascript"></script>
+
+		<!-- custom js -->
+		<script src="js/custom.js" type="text/javascript"></script>
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+		<title>Ryan Henson</title>
+	</head>
+	<body data-spy="scroll" data-target=".navbar-fixed-top">
+		<img id="egggif" src="https://media.giphy.com/media/DpXqHdILXRRDi/giphy.gif"/>
+		<header>
+			<div class="container-fluid">
+				<nav class="navbar navbar-default navbar-fixed-top">
+					<div class="navbar-header">
+						<!-- this is the mobile menu button -->
+						<button class="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target="#main-menu">
+							<span class="sr-only">main menu</span>
+							<span class="glyphicon glyphicon-menu-hamburger"></span>
+						</button>
+					</div>
+					<!-- Collect the nav links for toggling -->
+					<div class="collapse navbar-collapse" id="main-menu">
+						<ul class="nav navbar-nav navbar-left">
+							<li><a href="#">Home</a></li>
+							<li><a href="#section-2">About Me</a></li>
+							<li><a href="#section-3">Professional Experience</a></li>
+							<li><a href="#section-4">Contact</a></li>
+						</ul>
+						<ul class="nav navbar-nav navbar-right">
+							<li><a href="https://github.com/jryanhenson/" target="_blank"><i class="fa fa-github fa-lg" aria-hidden="true"></i></a></li>
+							<li><a href="https://www.linkedin.com/in/j-ryan-henson-4415a2117/" target="_blank"><i class="fa fa-linkedin fa-lg" aria-hidden="true"></i></a></li>
+						</ul>
+					</div><!-- /.navbar-collapse -->
+				</nav>
+			</div>
+		</header>
+		<main>
+			<section id="particleground">
+				<div class="container-fluid">
+					<div id="welcome">
+						<div class="row">
+							<div class="col-xs-4 col-md-2" >
+								<img src="https://www.fillmurray.com/300/300" class="img-responsive center-block img-rounded" alt="Responsive image">
+							</div>
+							<div class="col-xs-8 col-md-10" >
+								<h1>Ryan Henson</h1>
+								<h2>Information Security Consultant and Developer</h2>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+			<section id="section-2">
+				<div class="container-fluid">
+					<div class="row">
+						<div class="col-xs-12">
+							<h1>About Me</h1>
+							<p class="lead">Tote bag shabby chic locavore lumbersexual umami, subway tile gastropub messenger bag pork belly. Kogi narwhal scenester af, austin raw denim neutra cronut swag brooklyn retro polaroid palo santo. Four dollar toast bushwick af skateboard etsy craft beer freegan, taiyaki slow-carb wolf. Normcore master cleanse street art, 90's tilde enamel pin VHS williamsburg 8-bit tumeric. Gentrify gochujang keffiyeh, kinfolk small batch hot chicken helvetica. Plaid live-edge salvia, vice normcore unicorn gastropub. Hot chicken af poutine, thundercats echo park migas fanny pack literally +1 tumblr stumptown raclette viral.</p>
+							<p class="lead">Hell of venmo schlitz stumptown edison bulb. Narwhal blue bottle fashion axe succulents occupy gluten-free. Mlkshk pour-over marfa, normcore raclette prism copper mug photo booth neutra kickstarter letterpress leggings put a bird on it tofu. Flexitarian vegan air plant williamsburg, etsy marfa gochujang. Cronut jianbing DIY occupy humblebrag hoodie. Microdosing shabby chic cred keytar, raclette farm-to-table forage. Listicle chicharrones scenester squid.</p>
+							<p class="lead">Tote bag shabby chic locavore lumbersexual umami, subway tile gastropub messenger bag pork belly. Kogi narwhal scenester af, austin raw denim neutra cronut swag brooklyn retro polaroid palo santo. Four dollar toast bushwick af skateboard etsy craft beer freegan, taiyaki slow-carb wolf. Normcore master cleanse street art, 90's tilde enamel pin VHS williamsburg 8-bit tumeric. Gentrify gochujang keffiyeh, kinfolk small batch hot chicken helvetica. Plaid live-edge salvia, vice normcore unicorn gastropub. Hot chicken af poutine, thundercats echo park migas fanny pack literally +1 tumblr stumptown raclette viral.</p>
+						</div>
+					</div>
+				</div>
+			</section>	\
+			<section id="section-3">
+				<div class="container-fluid">
+					<div class="row">
+						<div class="col-xs-12">
+							<h1>Professional Experience</h1>
+							<p class="lead">Tote bag shabby chic locavore lumbersexual umami, subway tile gastropub messenger bag pork belly. Kogi narwhal scenester af, austin raw denim neutra cronut swag brooklyn retro polaroid palo santo. Four dollar toast bushwick af skateboard etsy craft beer freegan, taiyaki slow-carb wolf. Normcore master cleanse street art, 90's tilde enamel pin VHS williamsburg 8-bit tumeric. Gentrify gochujang keffiyeh, kinfolk small batch hot chicken helvetica. Plaid live-edge salvia, vice normcore unicorn gastropub. Hot chicken af poutine, thundercats echo park migas fanny pack literally +1 tumblr stumptown raclette viral.</p>
+							<p class="lead">Hell of venmo schlitz stumptown edison bulb. Narwhal blue bottle fashion axe succulents occupy gluten-free. Mlkshk pour-over marfa, normcore raclette prism copper mug photo booth neutra kickstarter letterpress leggings put a bird on it tofu. Flexitarian vegan air plant williamsburg, etsy marfa gochujang. Cronut jianbing DIY occupy humblebrag hoodie. Microdosing shabby chic cred keytar, raclette farm-to-table forage. Listicle chicharrones scenester squid.</p>
+							<p class="lead">Tote bag shabby chic locavore lumbersexual umami, subway tile gastropub messenger bag pork belly. Kogi narwhal scenester af, austin raw denim neutra cronut swag brooklyn retro polaroid palo santo. Four dollar toast bushwick af skateboard etsy craft beer freegan, taiyaki slow-carb wolf. Normcore master cleanse street art, 90's tilde enamel pin VHS williamsburg 8-bit tumeric. Gentrify gochujang keffiyeh, kinfolk small batch hot chicken helvetica. Plaid live-edge salvia, vice normcore unicorn gastropub. Hot chicken af poutine, thundercats echo park migas fanny pack literally +1 tumblr stumptown raclette viral.</p>
+						</div>
+					</div>
+				</div>
+			</section>
+			<section id="section-4">
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-12">
+						<!-- BEGIN CONTACT FORM -->
+						<form id="contact-form" method="POST" action="php/mailer.php">
+							<div class="form-group">
+								<label for="name">Name</label>
+								<div class="input-group">
+									<div class="input-group-addon">
+										<i class="fa fa-user"></i>
+									</div>
+									<input class="form-control" type="text" name="name" id="name" placeholder="Your Name">
+								</div>
+							</div>
+							<div class="form-group">
+								<label for="email">Email</label>
+								<div class="input-group">
+									<div class="input-group-addon">
+										<i class="fa fa-envelope"></i>
+									</div>
+									<input class="form-control" type="email" name="email" id="email" placeholder="Your Email">
+								</div>
+							</div>
+							<div class="form-group">
+								<label for="subject">Subject</label>
+								<div class="input-group">
+									<div class="input-group-addon">
+										<i class="fa fa-pencil"></i>
+									</div>
+									<input class="form-control" type="text" name="subject" id="subject" placeholder="Subject">
+								</div>
+							</div>
+							<div class="form-group">
+								<label for="message">Message</label>
+									<div class="input-group">
+									<div class="input-group-addon">
+										<i class="fa fa-comment"></i>
+									</div>
+									<textarea rows="5" name="message" id="message" class="form-control" placeholder="Your Message (2000 charaters max)"></textarea>
+								</div>
+							</div>
+						<!-- Google reCAPTCHA -->
+							<div class="g-recaptcha" data-sitekey="6Lf1zyMUAAAAAC8Dzz3pD0Lqclnqni7rtyXh9Kpe"></div>
+							<button class="btn btn-default" type="reset">Reset</button>
+							<button class="btn btn-info" type="submit">Submit</button>
+						</form>
+						<!-- END CONTACT FORM-->
+					</div><!-- /.col-sm-6 -->
+				</div><!-- /.row -->
+			<!-- EMPTY FORM OUTPUT AREA -->
+				<div class="row">
+					<div class="col-xs-12">
+						<div id="output-area"></div>
+					</div>
+				</div>
+			</div>
+			</section>
+		</main>
+	</body>
+		<footer>
+			<div class="container-fluid text-center">
+				This is the footer
+			</div>
+		</footer>
+</html>
