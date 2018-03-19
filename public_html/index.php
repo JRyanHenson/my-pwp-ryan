6412488726c22b18c918c8f4a5959b1ec07ac0b6
jryan42004@yahoo.com <5860db04b1ca48d6bfe8236e9ccb88abde0291ee>
2018-03-18 21:02:58 -0600

    Added google meta data for verification

--- public_html/index.php
+++ public_html/index.php
@@ -6,6 +6,7 @@
 		<meta name="description" content="This website aims to briefly describe Ryan Henson and his professional experience.">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="google-site-verification" content="vYvYPeEzOLrXpe7x8RBQziClR0_cUpjooG4mMODvlfc" />
 
 		<!-- Latest compiled and minified CSS -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
